bbc1f1ea8b5b849b75caf9da848957d8020e5608
IvanRave <reyvart@hotmail.com>
2013-06-30 13:02:36 +0300

    change model binding to '.'

--- scripts/app/project.js
+++ scripts/app/project.js
@@ -1,42 +1,45 @@
-﻿var app = angular.module("project", ['geodata']);
+﻿// Modules can list other modules as their dependencies.
+// ==============
+// Modules are a way of managing $injector configuration, and have nothing to do with loading of scripts into a VM. 
+// There are existing projects which deal with script loading, which may be used with Angular. 
+// Because modules do nothing at load time they can be loaded into the VM in any order 
+// and thus script loaders can take advantage of this property and parallelize the loading process.
+var app = angular.module('project', ['geodata']);
 
-app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/map', { controller: MapCtrl, templateUrl: '/map.html' })
-        .when('/geo', { controller: GeoCtrl, templateUrl: '/geo.html' })
-        .when('/create', { controller: CreateCtrl, templateUrl: '/create.html' })
-        .when('/login', { controller: LoginCtrl, templateUrl: '/login.html' })
-        .otherwise({ redirectTo: '/geo' });
+var client = new WindowsAzure.MobileServiceClient('https://rave-mobile.azure-mobile.net/', 'cJVLPsElKKsvBvtlYLKJwPgBzkFPmk65');
 
-    ////$locationProvider.html5Mode(true);
+LoginCtrl.$inject = ['$scope', '$rootScope'];
+function LoginCtrl(fncScope, fncRootScope) {
 
-    var client = new WindowsAzure.MobileServiceClient('https://rave-mobile.azure-mobile.net/', 'cJVLPsElKKsvBvtlYLKJwPgBzkFPmk65');
+    // todo: how to move to global?
+    ////$scope.isLoggedIn = client.currentUser !== null;
 
-    function GeoCtrl($scope, GeoData) {
-        $scope.geoPointList = GeoData.query();
-    }
+    fncScope.loginWithFacebook = function () {
+        ////if ($scope.isLoggedIn === true) { return; }
+
+        fncRootScope.isLoggedIn = true;
+        console.log(fncRootScope.isLoggedIn);
+
+        ////client.login('facebook').then(function (response) {
+        ////    console.log(response);
+        ////    console.log(client.currentUser);
+        ////    fncRootScope.isLoggedIn = true;
+        ////    fncRootScope.userName = client.currentUser;
+        ////}, function (error) {
+        ////    console.log(error);
+        ////});
+    };
+}
+
+app.config(['$routeProvider', function (rpr) {
+    var GeoCtrl = ['$scope', 'GeoData', function ($scope, GeoData) {
+        $scope.geoForm = { pointList: GeoData.query() };
+    }];
 
     function CreateCtrl($scope, $location, GeoData) {
         $scope.save = function () {
             GeoData.save($scope.project, function (project) {
                 $location.path('/geo');
-            });
-        }
-    }
-
-    function LoginCtrl($scope) {
-        // todo: how to move to global?
-        ////$scope.isLoggedIn = client.currentUser !== null;
-
-        $scope.loginWithFacebook = function () {
-            ////if ($scope.isLoggedIn === true) { return; }
-
-            client.login('facebook').then(function (response) {
-                console.log(response);
-                console.log(client.currentUser);
-            },
-            function (error) {
-                console.log(error);
             });
         };
     }
@@ -44,7 +47,7 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
     function MapCtrl($scope) {
         var myMap;
 
-        $scope.isMapLoaded = false;
+        $scope.mapForm = { isMapLoaded: false };
 
         $scope.initialize = function () {
             ymaps.ready(function () {
@@ -67,7 +70,7 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 
                 if (!$scope.$$phase) {
                     $scope.$apply(function () {
-                        $scope.isMapLoaded = true;
+                        $scope.mapForm.isMapLoaded = true;
                     });
                 }
 
@@ -102,8 +105,65 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
             });
         };
 
+        // Обработчик загрузки XML-файлов.
+        function onGeoXmlLoad(res, isLastMap) {
+            myCollection.add(res.geoObjects);
+            // Изменение области показа карты
+            //  myMap.setBounds(bounds);
+
+            if (isLastMap === true) {
+                console.log(myCollection.getLength());
+                ////myMap.geoObjects.add(myCollection);
+
+                // building routes
+                var routeCollection = new ymaps.GeoObjectCollection({}, {});
+                myCollection.each(function (elemValue) {
+                    /////console.log(elemValue.getLength());
+                    elemValue.each(function (objValue) {
+                        ////console.log(objValue.geometry.getCoordinates());
+                        ///
+                        var routeCoords = objValue.geometry.getCoordinates();
+
+                        ymaps.route([myMap.getCenter(), routeCoords]).then(function (route) {
+                            routeCollection.add(route);
+                            //    console.log(route);
+                            ////myMap.geoObjects.add(route);
+
+                            console.log(routeCollection.getLength());
+                            if (routeCollection.getLength() === 23) {
+                                myMap.geoObjects.add(routeCollection);
+                                ////http://api.yandex.ru/maps/doc/jsapi/2.x-rc/ref/reference/router.Route.xml
+                            }
+                        });
+                    });
+                });
+
+                //console.log(myCollection.get(0)); // Первый объект    
+            }
+            //if (res.mapState) {
+            //    res.mapState.applyToMap(myMap);
+            //}
+            // bounds - заданная область показа карты
+            // var bounds = res.mapState.getBounds();
+            // Добавление коллекции геообъектов на карту
+            ////myMap.geoObjects.add(res.geoObjects);
+        }
+
+        function onGeoXmlLoadError(error) {
+            // todo: handle error
+
+            alert(error);
+            /////$("#loader").html('<span style="color:red">Ошибка при загрузке YMapsML: ' + error + '</span>');
+        }
+
+        function loadToCollection(mapName, isLastMap) {
+            ymaps.geoXml.load("http://maps.yandex.ua/export/usermaps/" + mapName + "/").then(function (res) {
+                onGeoXmlLoad(res, isLastMap);
+            }, onGeoXmlLoadError);
+        }
+
         $scope.findPlace = function () {
-            if ($scope.isMapLoaded === true) {
+            if ($scope.mapForm.isMapLoaded === true) {
                 ymaps.geocode($scope.startPointName, { results: 1 }).then(function (res) {
                     var firstGeoObject = res.geoObjects.get(0);
                     if (firstGeoObject !== null) {
@@ -117,63 +177,6 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
                             preset: 'twirl#brownStretchyIcon',
                             geoObjectCursor: 'point'
                         });
-
-                        // Обработчик загрузки XML-файлов.
-                        function onGeoXmlLoad(res, isLastMap) {
-                            myCollection.add(res.geoObjects);
-                            // Изменение области показа карты
-                            //  myMap.setBounds(bounds);
-
-                            if (isLastMap === true) {
-                                console.log(myCollection.getLength());
-                                ////myMap.geoObjects.add(myCollection);
-
-                                // building routes
-                                var routeCollection = new ymaps.GeoObjectCollection({}, {});
-                                myCollection.each(function (elemValue) {
-                                    /////console.log(elemValue.getLength());
-                                    elemValue.each(function (objValue) {
-                                        ////console.log(objValue.geometry.getCoordinates());
-                                        ///
-                                        var routeCoords = objValue.geometry.getCoordinates();
-
-                                        ymaps.route([myMap.getCenter(), routeCoords]).then(function (route) {
-                                            routeCollection.add(route);
-                                            //    console.log(route);
-                                            ////myMap.geoObjects.add(route);
-
-                                            console.log(routeCollection.getLength());
-                                            if (routeCollection.getLength() === 23) {
-                                                myMap.geoObjects.add(routeCollection);
-                                                ////http://api.yandex.ru/maps/doc/jsapi/2.x-rc/ref/reference/router.Route.xml
-                                            }
-                                        });
-                                    });
-                                });
-
-                                //console.log(myCollection.get(0)); // Первый объект    
-                            }
-                            //if (res.mapState) {
-                            //    res.mapState.applyToMap(myMap);
-                            //}
-                            // bounds - заданная область показа карты
-                            // var bounds = res.mapState.getBounds();
-                            // Добавление коллекции геообъектов на карту
-                            ////myMap.geoObjects.add(res.geoObjects);
-                        }
-
-                        function onGeoXmlLoadError(error) {
-                            // todo: handle error
-
-                            alert(error);
-                            /////$("#loader").html('<span style="color:red">Ошибка при загрузке YMapsML: ' + error + '</span>');
-                        }
-
-                        function loadToCollection(mapName, isLastMap) {
-                            ymaps.geoXml.load("http://maps.yandex.ua/export/usermaps/" + mapName + "/").then(function (res) {
-                                onGeoXmlLoad(res, isLastMap);
-                            }, onGeoXmlLoadError);
-                        }
 
                         ////var mapNameList = ["EraqyPO94sOvKxc6kNQ2XbMHO7EJJR0R", "t3pjobj4S1hssyQjYPRKI1DGSCQkU9iI", "JBAz7LI1otiUXlo8Kq8WJtF_w2y7pb_5"];
                         var mapNameList = ["EraqyPO94sOvKxc6kNQ2XbMHO7EJJR0R", "t3pjobj4S1hssyQjYPRKI1DGSCQkU9iI"];
@@ -195,13 +198,29 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
             }
         };
     }
+
+    rpr.when('/map', { controller: MapCtrl, templateUrl: '/map.html' })
+        .when('/geo', { controller: GeoCtrl, templateUrl: '/geo.html' })
+        .when('/create', { controller: CreateCtrl, templateUrl: '/create.html' })
+        .when('/login', { controller: LoginCtrl, templateUrl: '/login.html' })
+        .otherwise({ redirectTo: '/map' });
+
+    ////$locationProvider.html5Mode(true);
 }]);
 
-// http://stackoverflow.com/questions/10486769/cannot-get-to-rootscope
 app.run(function ($rootScope) {
-    var client = new WindowsAzure.MobileServiceClient('https://rave-mobile.azure-mobile.net/', 'cJVLPsElKKsvBvtlYLKJwPgBzkFPmk65');
+    //var client = new WindowsAzure.MobileServiceClient('https://rave-mobile.azure-mobile.net/', 'cJVLPsElKKsvBvtlYLKJwPgBzkFPmk65');
     $rootScope.isLoggedIn = client.currentUser !== null;
+
+    // console.log(GeoCtrl);
+    ////GeoCtrl.$inject(['$scope', 'GeoData']);
 
     console.log(client.currentUser);
     console.log('hello');
 });
+// =========== config block ===========
+// Only providers and constants can be injected into configuration blocks
+// http://stackoverflow.com/questions/10486769/cannot-get-to-rootscope
+// =========== run block =======
+// Only instances and constants can be injected into run blocks. This is to prevent further system configuration during application run time.
+// It is executed after all of the service have been configured and the injector has been created
