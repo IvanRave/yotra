e9a3b26381cdfe62030596a80f0690bb419ba9e0
IvanRave <reyvart@hotmail.com>
2013-07-24 20:19:56 +0300

    Update jquery

--- scripts/_references.js
+++ scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-2.0.2.js" />
+﻿/// <reference path="jquery-2.0.3.js" />
 /// <reference path="bootstrap.js" />
 /// <reference path="angular.js" />
 /// <reference path="leaflet.js" />
