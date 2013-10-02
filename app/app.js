a20f06b1077fa5b3e5b0337602e61f32f80e7d6b
IvanRave <reyvart@hotmail.com>
2013-10-02 23:46:23 +0300

    Change style; Add google verif

--- app/app.js
+++ app/app.js
@@ -2,8 +2,8 @@
     .config(['$routeProvider', '$locationProvider', function (angRouteProvider, angLocationProvider) {
         angRouteProvider
             .when('/', { controller: 'MenuCtrl', templateUrl: '/app/menu/main.tpl.html' })
-            .when('/ticket', { controller: 'MenuCtrl', templateUrl: '/app/ticket/main.tpl.html' })
-            //.when('/map', { controller: 'MenuCtrl', templateUrl: '/app/map/main.tpl.html' })
+            .when('/tickets', { controller: 'MenuCtrl', templateUrl: '/app/ticket/main.tpl.html' })
+            .when('/maps', { controller: 'MenuCtrl', templateUrl: '/app/map/main.tpl.html' })
             ////.when('/forum/list', { controller: 'ForumListCtrl', templateUrl: '/app/forum/list.tpl.html' })
             ////.when('/forum/post', { controller: 'ForumPostCtrl', templateUrl: '/app/forum/post.tpl.html' })
             .otherwise({ redirectTo: '/' });
