68220c99c5f3ef3f8c1f7a0d6f878ceb8be5ce40
IvanRave <reyvart@hotmail.com>
2013-10-03 08:43:52 +0300

    Change template to main files

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
 ﻿angular.module('app', ['ngRoute', 'ang-menu-controllers'])
     .config(['$routeProvider', '$locationProvider', function (angRouteProvider, angLocationProvider) {
         angRouteProvider
-            .when('/', { controller: 'MenuCtrl', templateUrl: '/app/menu/main.tpl.html' })
-            .when('/tickets', { controller: 'MenuCtrl', templateUrl: '/app/ticket/main.tpl.html' })
-            .when('/maps', { controller: 'MenuCtrl', templateUrl: '/app/map/main.tpl.html' })
+            .when('/', { controller: 'MenuCtrl', templateUrl: '/app/menu/' })
+            .when('/app/tickets/', { controller: 'MenuCtrl', templateUrl: '/app/tickets/' })
+            .when('/app/maps/', { controller: 'MenuCtrl', templateUrl: '/app/maps/' })
             ////.when('/forum/list', { controller: 'ForumListCtrl', templateUrl: '/app/forum/list.tpl.html' })
             ////.when('/forum/post', { controller: 'ForumPostCtrl', templateUrl: '/app/forum/post.tpl.html' })
             .otherwise({ redirectTo: '/' });
