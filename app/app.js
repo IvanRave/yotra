f8d5119f1b9537c2d5090511e861c5de0b575a08
IvanRave <reyvart@hotmail.com>
2013-10-03 11:48:33 +0300

    Add review link to route

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@
             .when('/', { controller: 'MenuCtrl', templateUrl: '/app/menu/' })
             .when('/app/tickets/', { controller: 'MenuCtrl', templateUrl: '/app/tickets/' })
             .when('/app/maps/', { controller: 'MenuCtrl', templateUrl: '/app/maps/' })
+            .when('/app/reviews/', { controller: 'MenuCtrl', templateUrl: '/app/reviews/' })
             ////.when('/forum/list', { controller: 'ForumListCtrl', templateUrl: '/app/forum/list.tpl.html' })
             ////.when('/forum/post', { controller: 'ForumPostCtrl', templateUrl: '/app/forum/post.tpl.html' })
             .otherwise({ redirectTo: '/' });
