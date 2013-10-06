0d7500e04acb07db4387e8f43fa69bec2afe52fb
IvanRave <reyvart@hotmail.com>
2013-10-07 01:02:46 +0300

    Updates

--- app/bundle-0.1.0.js
+++ app/bundle-0.1.0.js
@@ -1,3 +1,23 @@
 /*! yotra - v0.1.0 - 2013-10-07
 * http://yotra.ru
 * Copyright (c) 2013 IvanRave; Licensed MIT */
+angular.module('ang-menu-controllers', [])
+    .controller('MenuCtrl', ['$scope', function () {
+        'use strict';
+
+    }]);;/* some header comment in app.js */
+angular.module('app', ['ngRoute', 'ang-menu-controllers'])
+    .config(['$routeProvider', '$locationProvider', function (angRouteProvider, angLocationProvider) {
+        angRouteProvider
+            .when('/', { controller: 'MenuCtrl', templateUrl: '/app/menu/' })
+            .when('/app/tickets/', { controller: 'MenuCtrl', templateUrl: '/app/tickets/' })
+            .when('/app/maps/', { controller: 'MenuCtrl', templateUrl: '/app/maps/' })
+            .when('/app/reviews/', { controller: 'MenuCtrl', templateUrl: '/app/reviews/' })
+            ////.when('/forum/list', { controller: 'ForumListCtrl', templateUrl: '/app/forum/list.tpl.html' })
+            ////.when('/forum/post', { controller: 'ForumPostCtrl', templateUrl: '/app/forum/post.tpl.html' })
+            .otherwise({ redirectTo: '/' });
+
+        angLocationProvider.html5Mode(true).hashPrefix('!');
+    }])
+    .run(['$rootScope', function () {
+    }]);
