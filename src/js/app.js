479a5498c967a8b98f28d3e71dab9b203a37ace5
IvanRave <reyvart@hotmail.com>
2013-10-11 01:45:46 +0300

    Build version

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+﻿/* some header comment in app.js */
+angular.module('app', ['ngRoute', 'ang-menu-controllers'])
+    .config(['$routeProvider', '$locationProvider', function (angRouteProvider, angLocationProvider) {
+        angRouteProvider
+            .when('/', { controller: 'MenuCtrl', templateUrl: '/js/menu/' })
+            .when('/app/tickets/', { controller: 'MenuCtrl', templateUrl: '/js/tickets/' })
+            ////.when('/maps.html', { controller: 'MenuCtrl', templateUrl: '/maps.html' })
+            .when('/app/reviews/', { controller: 'MenuCtrl', templateUrl: '/js/reviews/' })
+            ////.when('/forum/list', { controller: 'ForumListCtrl', templateUrl: '/app/forum/list.tpl.html' })
+            ////.when('/forum/post', { controller: 'ForumPostCtrl', templateUrl: '/app/forum/post.tpl.html' })
+            .otherwise({ redirectTo: '/' });
+
+        angLocationProvider.html5Mode(true).hashPrefix('!');
+    }])
+    .run(['$rootScope', function () {
+    }]);
