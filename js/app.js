fffad06c133b188a05121acab616d92e4a7d945a
IvanRave <reyvart@hotmail.com>
2013-10-10 22:30:36 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@
 angular.module('app', ['ngRoute', 'ang-menu-controllers'])
     .config(['$routeProvider', '$locationProvider', function (angRouteProvider, angLocationProvider) {
         angRouteProvider
-            .when('/', { controller: 'MenuCtrl', templateUrl: '/app/menu/' })
-            .when('/app/tickets/', { controller: 'MenuCtrl', templateUrl: '/app/tickets/' })
-            .when('/maps.html', { controller: 'MenuCtrl', templateUrl: '/maps.html' })
-            .when('/app/reviews/', { controller: 'MenuCtrl', templateUrl: '/app/reviews/' })
+            .when('/', { controller: 'MenuCtrl', templateUrl: '/js/menu/' })
+            .when('/app/tickets/', { controller: 'MenuCtrl', templateUrl: '/js/tickets/' })
+            ////.when('/maps.html', { controller: 'MenuCtrl', templateUrl: '/maps.html' })
+            .when('/app/reviews/', { controller: 'MenuCtrl', templateUrl: '/js/reviews/' })
             ////.when('/forum/list', { controller: 'ForumListCtrl', templateUrl: '/app/forum/list.tpl.html' })
             ////.when('/forum/post', { controller: 'ForumPostCtrl', templateUrl: '/app/forum/post.tpl.html' })
             .otherwise({ redirectTo: '/' });
