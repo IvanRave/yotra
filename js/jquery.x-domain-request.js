17b7e0f23f899cfe187c803e76c970bb535c5fe8
IvanRave <reyvart@hotmail.com>
2013-10-13 22:46:52 +0300

    Updates

--- js/jquery.x-domain-request.js
+++ js/jquery.x-domain-request.js
@@ -1,86 +1,40 @@
-// jQuery.XDomainRequest.js
-// Author: Jason Moon - @JSONMOON
-// IE8+
-if (!jQuery.support.cors && window.XDomainRequest) {
-	var httpRegEx = /^https?:\/\//i;
-	var getOrPostRegEx = /^get|post$/i;
-	var sameSchemeRegEx = new RegExp('^'+location.protocol, 'i');
-	var xmlRegEx = /\/xml/i;
-	
-	// ajaxTransport exists in jQuery 1.5+
-	jQuery.ajaxTransport('text html xml json', function(options, userOptions){
-		// XDomainRequests must be: asynchronous, GET or POST methods, HTTP or HTTPS protocol, and same scheme as calling page
-		if (options.crossDomain && options.async && getOrPostRegEx.test(options.type) && httpRegEx.test(userOptions.url) && sameSchemeRegEx.test(userOptions.url)) {
-			var xdr = null;
-			var userType = (userOptions.dataType||'').toLowerCase();
-			return {
-				send: function(headers, complete){
-					xdr = new XDomainRequest();
-					if (/^\d+$/.test(userOptions.timeout)) {
-						xdr.timeout = userOptions.timeout;
-					}
-					xdr.ontimeout = function(){
-						complete(500, 'timeout');
-					};
-					xdr.onload = function(){
-						var allResponseHeaders = 'Content-Length: ' + xdr.responseText.length + '\r\nContent-Type: ' + xdr.contentType;
-						var status = {
-							code: 200,
-							message: 'success'
-						};
-						var responses = {
-							text: xdr.responseText
-						};
-						/*
-						if (userType === 'html') {
-							responses.html = xdr.responseText;
-						} else
-						*/
-						try {
-							if (userType === 'json') {
-								try {
-									responses.json = JSON.parse(xdr.responseText);
-								} catch(e) {
-									status.code = 500;
-									status.message = 'parseerror';
-									//throw 'Invalid JSON: ' + xdr.responseText;
-								}
-							} else if ((userType === 'xml') || ((userType !== 'text') && xmlRegEx.test(xdr.contentType))) {
-								var doc = new ActiveXObject('Microsoft.XMLDOM');
-								doc.async = false;
-								try {
-									doc.loadXML(xdr.responseText);
-								} catch(e) {
-									doc = undefined;
-								}
-								if (!doc || !doc.documentElement || doc.getElementsByTagName('parsererror').length) {
-									status.code = 500;
-									status.message = 'parseerror';
-									throw 'Invalid XML: ' + xdr.responseText;
-								}
-								responses.xml = doc;
-							}
-						} catch(parseMessage) {
-							throw parseMessage;
-						} finally {
-							complete(status.code, status.message, responses, allResponseHeaders);
-						}
-					};
-					xdr.onerror = function(){
-						complete(500, 'error', {
-							text: xdr.responseText
-						});
-					};
-					xdr.open(options.type, options.url);
-					//xdr.send(userOptions.data);
-					xdr.send();
-				},
-				abort: function(){
-					if (xdr) {
-						xdr.abort();
-					}
-				}
-			};
-		}
-	});
+if (window.XDomainRequest) {
+    jQuery.ajaxTransport(function (s) {
+        if (s.crossDomain && s.async) {
+            if (s.timeout) {
+                s.xdrTimeout = s.timeout;
+                delete s.timeout;
+            }
+            var xdr;
+            return {
+                send: function (_, complete) {
+                    function callback(status, statusText, responses, responseHeaders) {
+                        xdr.onload = xdr.onerror = xdr.ontimeout = jQuery.noop;
+                        xdr = undefined;
+                        complete(status, statusText, responses, responseHeaders);
+                    }
+                    xdr = new XDomainRequest();
+                    xdr.onload = function () {
+                        callback(200, "OK", { text: xdr.responseText }, "Content-Type: " + xdr.contentType);
+                    };
+                    xdr.onerror = function () {
+                        callback(404, "Not Found");
+                    };
+                    xdr.onprogress = jQuery.noop;
+                    xdr.ontimeout = function () {
+                        callback(0, "timeout");
+                    };
+                    xdr.timeout = s.xdrTimeout || Number.MAX_VALUE;
+                    xdr.open(s.type, s.url);
+                    xdr.send((s.hasContent && s.data) || null);
+                },
+                abort: function () {
+                    if (xdr) {
+                        xdr.onerror = jQuery.noop;
+                        xdr.abort();
+                    }
+                }
+            };
+        }
+    });
 }
