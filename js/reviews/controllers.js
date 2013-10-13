66ede9518ad8df266dae7f4eb17d27ac9b938526
IvanRave <reyvart@hotmail.com>
2013-10-13 22:20:10 +0300

    Updates

--- js/reviews/controllers.js
+++ js/reviews/controllers.js
@@ -1,15 +1,20 @@
 ﻿angular.module('controllers', ['ngResource'])
-    .controller('PlaceReviewsCtrl', ['$scope', '$http', '$resource', function (angScope, angHttp, angResource) {
+    .controller('PlaceReviewsCtrl', ['$scope', function (angScope) {
         'use strict';
 
         angScope.placeList = [];
 
-        angHttp.defaults.useXDomain = true;
-
-        var placeResource = angResource('http://yotra.azurewebsites.net/api/reviews/', {});
-        placeResource.get({}, function (res) {
+        jQuery.ajax({ url: 'http://yotra.azurewebsites.net/api/reviews/' }).done(function (res) {
+            console.log(res);
             angScope.placeList = res["places"];
         });
+
+        ////angHttp.defaults.useXDomain = true;
+
+        ////var placeResource = angResource('http://yotra.azurewebsites.net/api/reviews/', {});
+        ////placeResource.get({}, function (res) {
+        ////    angScope.placeList = res["places"];
+        ////});
         ////placeReviewsFactory.get({}, function (res) {
         ////    angScope.placeList = res["places"];
         ////});
